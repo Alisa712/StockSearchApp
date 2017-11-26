32ef6c4a6b43ac24fda2fd887985517b8b29aea8
Shuhui(Alisa) He <hsh0712@hotmail.com>
2017-11-26 12:31:21 -0800

    -

--- app/src/main/java/com/example/shuhuihe/myapplication/Stock_detail_curr.java
+++ app/src/main/java/com/example/shuhuihe/myapplication/Stock_detail_curr.java
@@ -7,14 +7,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.webkit.ValueCallback;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
-import android.widget.AdapterView;
-import android.widget.Button;
 import android.widget.ListView;
-import android.widget.Spinner;
 
 import com.android.volley.DefaultRetryPolicy;
 import com.android.volley.Request;
@@ -38,13 +31,12 @@ import static java.lang.Integer.parseInt;
 public class Stock_detail_curr extends Fragment {
     private View rootview;
     private ListView listview;
-    private WebView mWebView;
     private String symbol;
     private final String URL = "http://shuhuihe571hw8-env.us-east-2.elasticbeanstalk.com/stock/query?function=TIME_SERIES_DAILY&outputsize=full&symbol=";
     RequestQueue queue;
     private JSONObject timeSeriesDaily;
+    //private JSONObject metaData;
     private JSONArray jsonArray;
-    private String currentIndi;
 
 
     @Override
@@ -63,8 +55,8 @@ public class Stock_detail_curr extends Fragment {
             String symbolTemp = getActivity().getIntent().getExtras().getString("symbol");
             symbol = symbolTemp.split("-")[0];
             //Log.e("stock name",symbol);
-            requestStockData(symbol);
 
+<<<<<<< HEAD
             Spinner spinner = (Spinner) rootview.findViewById(R.id.change_chart);
             spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
                 @Override
@@ -94,31 +86,14 @@ public class Stock_detail_curr extends Fragment {
             });
 
             loadWebView(symbol, currentIndi);
+=======
+
+            requestStockData(symbol);
+>>>>>>> parent of 21e5414... changing webview
 
 
         }
         return rootview;
-    }
-
-    private void loadWebView(final String symbol, String currentIndi) {
-        WebSettings webSettings = mWebView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-        webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
-
-        mWebView.loadUrl("file:///android_asset/huihui.html");
-        //mWebView.addJavascriptInterface(new IJavascriptHandler(), "chartJS");
-        mWebView.setWebViewClient(new WebViewClient() {
-            @Override
-            public void onPageFinished(WebView view, String url) {
-                super.onPageFinished(view, url);
-                mWebView.evaluateJavascript("javascript:loadPrice('" + symbol + "')", new ValueCallback<String>() {
-                    @Override
-                    public void onReceiveValue(String s) {
-
-                    }
-                });
-            }
-        });
     }
 
     private void requestStockData(String symbol) {
@@ -231,6 +206,7 @@ public class Stock_detail_curr extends Fragment {
 
         jsonObjReq.setRetryPolicy(new DefaultRetryPolicy(5000,DefaultRetryPolicy.DEFAULT_MAX_RETRIES,DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
         queue.add(jsonObjReq);
+
     }
 
 }
