eb8ead827d3328c71e84f816b749e70a77bada9a
Shuhui(Alisa) He <hsh0712@hotmail.com>
2017-11-26 22:57:47 -0800

    finished historical

--- app/src/main/java/com/example/shuhuihe/myapplication/stock_detail_his.java
+++ app/src/main/java/com/example/shuhuihe/myapplication/stock_detail_his.java
@@ -1,20 +1,22 @@
 package com.example.shuhuihe.myapplication;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
 
 
 public class Stock_detail_his extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-
-    public Stock_detail_his() {
-        // Required empty public constructor
-    }
-
+    private View rootview;
+    private WebView mWebView;
+    private String symbol;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -24,8 +26,32 @@ public class Stock_detail_his extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_stock_detail_his, container, false);
+        if (rootview == null) {
+
+            rootview = inflater.inflate(R.layout.fragment_stock_detail_his, container, false);
+            mWebView = rootview.findViewById(R.id.his_webview);
+            String symbolTemp = getActivity().getIntent().getExtras().getString("symbol");
+            String chartType = "historical";
+            symbol = symbolTemp.split("-")[0];
+            mWebView = rootview.findViewById(R.id.his_webview);
+            loadWebView(symbol, chartType);
+        }
+        return rootview;
     }
 
+    @SuppressLint("SetJavaScriptEnabled")
+    private void loadWebView(String symbol, String function) {
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
+        mWebView.loadUrl("file:///android_asset/huihui.html?" + symbol.trim() + "&" + function);
+        mWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+            }
+        });
+    }
 }
+
+
